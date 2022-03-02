4f79dbe8c5b017430597ae9af8aaa0d261d66fea
SpencerKing7 <spencerking.c@gmail.com>
2022-03-01 20:04:06 -0700

    mission 9 almost

---
+++ OnlineBookstore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineBookstore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineBookstore.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private SessionBasket cart;
+        public CartSummaryViewComponent (SessionBasket cartService)
+        {
+            cart = cartService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
